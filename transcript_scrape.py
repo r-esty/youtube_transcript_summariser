c9f7164bdf0f5c99df377d49722d2444f5a96930
r-esty <romeodesty@gmail.com>
2025-12-08 03:00:17 +0000

    Added Transcript feature

--- transcript_scrape.py
+++ transcript_scrape.py
@@ -1,22 +1,28 @@
 from flask import Flask, render_template, request
 import json
-
 import os
-from dotenv import load_dotenv,dotenv_values
+from dotenv import load_dotenv, dotenv_values
+from youtube_transcript_api import YouTubeTranscriptApi
 
 app = Flask(__name__)
+load_dotenv()
+ytt_api = YouTubeTranscriptApi()
 
 @app.route("/", methods=["POST", "GET"])
 def link():
 
-    if request.method == "POST" :
+    transcript = None
+    
+    
+    if request.method == "POST":
         link = request.form.get("link")
-        print(link)
-       
+    try:
+        video_id = link.replace("https://www.youtube.com/watch?v=","")
+        transcript = ytt_api.fetch(video_id)
+    except Exception as e:
+        error = str(e)
+    
+    return render_template("index.html", transcript=transcript)
 
-    return render_template("index.html")
-
-load_dotenv()
-
-print(os.getenv("api_key")) 
-#
+if __name__ == "__main__":
+    app.run(debug=True)
