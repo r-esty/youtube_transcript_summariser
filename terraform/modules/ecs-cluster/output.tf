72c199e817b6d155a175e60161c48a2b2ab79d8c
r-esty <romeodesty@gmail.com>
2025-12-16 01:34:08 +0000

    Add ecs-cluster module

---
+++ terraform/modules/ecs-cluster/output.tf
@@ -0,0 +1,4 @@
+output "ecs_cluster_id" {
+  description = "ID of the ECS Cluster"
+  value       = aws_ecs_cluster.ecs_cluster.id
+}
---
+++ terraform/modules/ecs-cluster/variable.tf
@@ -0,0 +1,5 @@
+variable "app_name" {
+    type = string
+    description = "The name of application"
+    default = "youtube_transcript"
+}
