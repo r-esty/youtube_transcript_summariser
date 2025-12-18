ca66e7bea21a289897205b510fc8a06a960ca11e
r-esty <romeodesty@gmail.com>
2025-12-18 23:50:34 +0000

    Fix Flask app

--- transcript_scrape.py
+++ transcript_scrape.py
@@ -9,28 +9,31 @@ load_dotenv()
 client = OpenAI(
     api_key=os.getenv("OPENAI_API_KEY")
 )
-
 app = Flask(__name__)
 ytt_api = YouTubeTranscriptApi()
 
 @app.route("/", methods=["POST", "GET"])
 def link():
     transcript = None
+    summary = None
+    
     if request.method == "POST":
-        link = request.form.get("link")
+        youtube_link = request.form.get("link")
         try:
-            video_id = link.replace("https://www.youtube.com/watch?v=","")
+            video_id = youtube_link.replace("https://www.youtube.com/watch?v=","").split("&")[0]
             transcript = ytt_api.fetch(video_id)
+            
+            response = client.responses.create(
+                model="gpt-4o",
+                input=f"Give a short but detailed summary of {transcript}. However if the transcript is none then just greet and ask them to give a Youtube link with transcript use the word transcript enabled so that you can provide a summary of the Youtube video"
+            )
+            summary = response.output_text
+            print(response.output_text)
         except Exception as e:
-            error = str(e)
-        
-        response = client.responses.create(
-            model="gpt-5.1",
-            input= f" Give a short but detailed summary of {transcript} however if the transcript is none then just greet and ask them to give a Youtube link with transcript use the word transcript enabled so that you can the provide a summary of the Youtube video"
-        )
-        summary = response.output_text
-        print(response.output_text)
-        return render_template("index.html", transcript=transcript, summary=summary, response=response)
+            print(f"Error: {e}")
+            summary = "Hi! Please share a YouTube link **that has transcript enabled**, and I'll give you a short but detailed summary of the video."
+    
+    return render_template("index.html", transcript=transcript, summary=summary)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
