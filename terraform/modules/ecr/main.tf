0597abe16bc1c88f83a568404857516c79823365
r-esty <romeodesty@gmail.com>
2026-02-15 23:16:26 +0000

    Change the the image_tag_mutability variable from IMMUTABLE to MUTABLE

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "main" {
   name                 = "${var.app_name}_ecr"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
   force_delete         = true
 
   image_scanning_configuration {
